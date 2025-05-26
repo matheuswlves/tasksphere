fd94012f02b18470395484a2c92a38dbf94e75bf
Matheus Lima <matheuslimaqueen@gmail.com>
2025-05-26 17:29:28 -0300

    consertando rota

--- src/routes/ProtectedRoute.js
+++ src/routes/ProtectedRoute.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
-import { useAuth } from 'C:/Users/mathe/tasksphere/src/contexts/AuthContext.js';
+import { useAuth } from '../contexts/AuthContext';
 
 const ProtectedRoute = () => {
     const { isAuthenticated, isLoading } = useAuth(); 
