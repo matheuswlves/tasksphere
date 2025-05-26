9e310fb0e0f3ccf30e40cabc31b5702ab8145603
Matheus Lima <matheuslimaqueen@gmail.com>
2025-05-26 18:27:17 -0300

    alterando Auth Context

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const AuthContext = createContext(null);
 
-const API_URL = 'http://localhost:3001'; 
+const API_URL = 'https://my-json-server.typicode.com/matheuswlves/tasksphere'; 
 
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
